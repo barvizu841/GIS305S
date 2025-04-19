9cd77136927e9945f3f4d89cb53f1d7516339c84
bearvizu <125218751+bearvizu@users.noreply.github.com>
2025-04-18 21:17:39 -0600

    Lab 2

---
+++ Lab2.2.py
@@ -0,0 +1,97 @@
+import yaml
+import arcpy
+from etl.GSheetsEtl import GSheetsEtl
+
+def setup():
+    with open('config/wnvoutbreak.yaml') as f:
+        config_dict = yaml.load(f, Loader=yaml.FullLoader)
+    return config_dict
+
+def bufferFunct (shapefile, bufferDistance):
+    outputName = f"{shapefile}_buffer"
+
+    if arcpy.Exists(outputName):
+        print(f"Output {outputName} already exists. Deleting it...")
+        arcpy.Delete_management(outputName)
+
+    arcpy.analysis.Buffer(shapefile, outputName, bufferDistance)
+    print(f"buffer executed for {shapefile}")
+    print(f"buffered shapefile: {outputName}")
+    return
+
+def intersectFunct (infeatures):
+    layerName = ("intersect")
+        #input(f"Name the intersect layer: "))
+    arcpy.analysis.Intersect(infeatures, layerName, "ONLY_FID")
+    print(f"{layerName} created")
+    return layerName
+
+# ETL Function
+def etl():
+    print("Starting ETL Process...")
+    etl_instance = GSheetsEtl(config_dict)
+    etl_instance.process()
+
+if __name__ == "__main__":
+    global config_dict
+    config_dict = setup()
+    print(config_dict)
+
+    #etl()
+
+    workspace = f"{config_dict.get('workspace_dir')}WestNileOutbreak.gdb" #updated to yaml
+    arcpy.env.workspace = workspace
+    arcpy.env.overwriteOutput = True
+
+    fcs = arcpy.ListFeatureClasses()
+    print(fcs)
+
+    bufferList = ["Wetlands", "OSMP_Properties", "Mosquito_Larval_Sites", "Lakes_and_Reservoirs", "avoid_points"]
+
+    for i in bufferList:
+        print(f"buffer:{i}")
+        #bufferDistance = input("Enter the buffer distance:") #set manually
+        bufferFunct(i, "1500 feet")
+
+    intersectLyr = intersectFunct(["Wetlands_buffer", "OSMP_Properties_buffer", "Mosquito_Larval_Sites_buffer", "Lakes_and_Reservoirs_buffer"])
+
+    #Too many features. Must Dissolve
+    print("Running Dissolve")
+    arcpy.management.Dissolve(
+        in_features="intersect",
+        out_feature_class="Intersect_Dissolve",
+        dissolve_field="FID_Wetlands_buffer",
+        statistics_fields=None,
+        multi_part="MULTI_PART",
+        unsplit_lines="DISSOLVE_LINES",
+        concatenation_separator=""
+    )
+
+    #erase
+    print("Running Erase")
+    arcpy.analysis.Erase("Intersect_Dissolve", "avoid_points_buffer", "spray_area")
+    print("Erase Executed")
+
+    arcpy.analysis.SpatialJoin("Addresses", "spray_area", "addresses_intersect_join")
+    join_lyr = "addresses_intersect_join"
+    print(f"{join_lyr} created")
+
+    proj_path = config_dict.get('workspace_dir')
+    aprx = arcpy.mp.ArcGISProject(rf"{proj_path}\\WestNileOutbreak.aprx")
+
+    map_doc = aprx.listMaps()[0]
+    #failed to add data. Possible credentials issue.
+    map_doc.addDataFromPath(rf"{config_dict.get('workspace_dir')}WestNileOutbreak.gdb\\{join_lyr}")
+    print(f"{join_lyr} added to aprx")
+    aprx.save()
+
+    column_name = "OID_1"
+    count_not_null = 0
+
+    with arcpy.da.SearchCursor(join_lyr, [column_name]) as cursor:
+        for row in cursor:
+            if row[0] is not None:
+                count_not_null += 1
+
+
+    print(f"Number of records where {column_name} is not null: {count_not_null}")
---
+++ etl/GSheetsEtl.py
@@ -0,0 +1,61 @@
+import arcpy
+import requests
+import csv
+from etl.SpatialEtl import SpatialEtl
+
+class GSheetsEtl(SpatialEtl):
+
+    config_dict = None
+
+    def __init__(self, config_dict):
+        super().__init__(config_dict)
+
+    def extract(self):
+        print("Extracting addresses from google form spreadsheet")
+
+        r = requests.get(self.config_dict.get('remote_url')) #updated to yaml
+        r.encoding = "utf-8"
+        data = r.text
+        with open(f"{self.config_dict.get('proj_dir')}addresses.csv", "w") as output_file:
+            output_file.write(data) #updated to yaml
+
+    def transform(self):
+        print("Add City, State")
+
+        transformed_file = open(f"{self.config_dict.get('proj_dir')}new_addresses.csv", "w") #updated to yaml
+        transformed_file.write("X, Y, Type\n")
+        with open(f"{self.config_dict.get('proj_dir')}addresses.csv", "r") as partial_file: #updated to yaml
+            csv_dict = csv.DictReader(partial_file, delimiter=',')
+            for row in csv_dict:
+                address = row["Street Address"] + "Boulder CO"
+                print(address)
+                geocode_url = self.config_dict.get('geocoder_prefix_ref') + address + \
+                              self.config_dict.get('geocoder_suffix_ref') #upadted to yaml
+                r = requests.get(geocode_url)
+
+                resp_dict = r.json()
+                x = resp_dict['result']['addressMatches'][0]['coordinates']['x']
+                y = resp_dict['result']['addressMatches'][0]['coordinates']['y']
+                transformed_file.write(f"{x}, {y},Residential\n")
+
+        transformed_file.close()
+
+    def load(self):
+        workspace = f"{self.config_dict.get('workspace_dir')}WestNileOutbreak.gdb" #updated to yaml
+        arcpy.env.workspace = workspace
+        arcpy.env.overwriteOutput = True
+
+        in_table = f"{self.config_dict.get('proj_dir')}new_addresses.csv" #updated to yaml
+        out_feature_class = "avoid_points"
+        x_coords = "X"
+        y_coords = "Y"
+
+        arcpy.management.XYTableToPoint(in_table, out_feature_class, x_coords, y_coords)
+
+        print(arcpy.GetCount_management(out_feature_class))
+
+    def process(self):
+        self.extract()
+        self.transform()
+        self.load()
+        
