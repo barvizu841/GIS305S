81480b197948759c8cef97c429add81745395245
bearvizu <125218751+bearvizu@users.noreply.github.com>
2025-04-22 08:28:09 -0600

    Lab 3 updates

--- Lab2.2.py
+++ Lab2.2.py
@@ -1,10 +1,16 @@
+from turtle import config_dict
+
 import yaml
 import arcpy
+import logging
+import os
 from etl.GSheetsEtl import GSheetsEtl
 
 def setup():
     with open('config/wnvoutbreak.yaml') as f:
         config_dict = yaml.load(f, Loader=yaml.FullLoader)
+        logging.basicConfig(filename=f"{config_dict.get('workspace_dir')}wnv.log",
+                            filemode="w", level=logging.DEBUG)
     return config_dict
 
 def bufferFunct (shapefile, bufferDistance):
@@ -32,31 +38,58 @@ def etl():
     etl_instance = GSheetsEtl(config_dict)
     etl_instance.process()
 
+def exportMap ():
+    aprx = arcpy.mp.ArcGISProject(f"{config_dict.get('workspace_dir')}WestNileOutbreak.aprx")
+    lyt = aprx.listLayouts()[0]
+    sub_title = input("Enter a subtitle for the map layout: ")
+    for el in lyt.listElements():
+        print(el.name)
+        if "Title" in el.name:
+            el.text = el.text + sub_title
+    lyt.exportToPDF(f"{config_dict.get('workspace_dir')}Layouts\WestNileOutbreakLayout.pdf")
+
 if __name__ == "__main__":
+
     global config_dict
     config_dict = setup()
     print(config_dict)
+    logging.info("Starting West Nile Virus Simulation")
 
+    # create folder for layouts
+    layout_folder = rf"{config_dict.get('workspace_dir')}Layouts"
+    os.makedirs(layout_folder, exist_ok=True)
+
+    logging.debug("Entering ETL Method")
     #etl()
+    logging.debug("Exiting ETL Method")
 
     workspace = f"{config_dict.get('workspace_dir')}WestNileOutbreak.gdb" #updated to yaml
     arcpy.env.workspace = workspace
     arcpy.env.overwriteOutput = True
 
     fcs = arcpy.ListFeatureClasses()
-    print(fcs)
+    logging.info(fcs)
+    #print(fcs)
 
+    logging.debug("Entering Buffer Method")
     bufferList = ["Wetlands", "OSMP_Properties", "Mosquito_Larval_Sites", "Lakes_and_Reservoirs", "avoid_points"]
 
+
     for i in bufferList:
-        print(f"buffer:{i}")
+        logging.info(f"buffer:{i}")
+        #print(f"buffer:{i}")
         #bufferDistance = input("Enter the buffer distance:") #set manually
         bufferFunct(i, "1500 feet")
+    logging.debug("Exiting Buffer Method")
 
+    logging.debug("Entering Intersect Method")
     intersectLyr = intersectFunct(["Wetlands_buffer", "OSMP_Properties_buffer", "Mosquito_Larval_Sites_buffer", "Lakes_and_Reservoirs_buffer"])
+    logging.debug("Exiting Intersect Method")
 
     #Too many features. Must Dissolve
-    print("Running Dissolve")
+    logging.info("Running Dissolve")
+    logging.debug("Entering Dissolve Method")
+    #print("Running Dissolve")
     arcpy.management.Dissolve(
         in_features="intersect",
         out_feature_class="Intersect_Dissolve",
@@ -66,25 +99,35 @@ if __name__ == "__main__":
         unsplit_lines="DISSOLVE_LINES",
         concatenation_separator=""
     )
+    logging.debug("Exiting Dissolve Method")
 
     #erase
-    print("Running Erase")
+    logging.debug("Entering Erase Method")
+    #print("Running Erase")
     arcpy.analysis.Erase("Intersect_Dissolve", "avoid_points_buffer", "spray_area")
-    print("Erase Executed")
+    logging.debug("Exiting Erase Method")
+    #print("Erase Executed")
 
+    logging.debug("Entering Spatial Join Method")
     arcpy.analysis.SpatialJoin("Addresses", "spray_area", "addresses_intersect_join")
     join_lyr = "addresses_intersect_join"
-    print(f"{join_lyr} created")
+    logging.info(f"{join_lyr} created")
+    #print(f"{join_lyr} created")
+    logging.debug("Exiting Spatial Join Method")
 
     proj_path = config_dict.get('workspace_dir')
     aprx = arcpy.mp.ArcGISProject(rf"{proj_path}\\WestNileOutbreak.aprx")
 
+    logging.debug("Entering Add Data Method")
     map_doc = aprx.listMaps()[0]
     #failed to add data. Possible credentials issue.
     map_doc.addDataFromPath(rf"{config_dict.get('workspace_dir')}WestNileOutbreak.gdb\\{join_lyr}")
-    print(f"{join_lyr} added to aprx")
+    logging.info(f"{join_lyr} added to aprx")
+    #print(f"{join_lyr} added to aprx")
     aprx.save()
+    logging.debug("Exiting Add Data Method")
 
+    logging.debug("Entering Count Records Method")
     column_name = "OID_1"
     count_not_null = 0
 
@@ -93,5 +136,9 @@ if __name__ == "__main__":
             if row[0] is not None:
                 count_not_null += 1
 
+    logging.info(f"Number of records where {column_name} is not null: {count_not_null}")
+    #print(f"Number of records where {column_name} is not null: {count_not_null}")
+    logging.debug("Exiting Count Records Method")
 
-    print(f"Number of records where {column_name} is not null: {count_not_null}")
+    #exportMap
+    exportMap()
